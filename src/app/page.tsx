c697229409d5cf1f03602b91214c51e7c78912d0
Amar Srivastava <amarsrivastava68@gmail.com>
2024-08-02 15:15:24 +0530

    Simple accordion component installed using Radix ui ;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
-
-     
-     <p>Hello , this is project starting</p>
+    <main className="absolute inset-0 flex justify-center items-center">
+      Website content
     </main>
   );
 }
