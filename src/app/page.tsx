3f7342de6a3a46d027adce33e87a1c7a8b2c6980
Amar Srivastava <amarsrivastava68@gmail.com>
2024-08-06 09:48:43 +0530

    added new components for header and input

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="absolute inset-0 flex justify-center items-center">
-      Website content
-    </main>
-  );
-}
